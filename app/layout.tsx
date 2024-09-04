a7e3663c4377eb46982dc4227361f96ee58c7088
Misha Lysak <myhaillysak@gmail.com>
2024-09-04 17:02:34 +0300

    [MAIN]: add meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts'
+import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Dashboard next js app',
+    default: 'Next js dashboard',
+  },
+  description: 'Hello Next js app, built with App Router.',
+  metadataBase: new URL('https://hello-next-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
